e64e9f5da55a9c0453b3ca27a15540aab45fc24b
Robert Shenton <theshenton@gmail.com>
2020-04-19 22:06:59 +0200

    Finalize build

--- src/me/miltage/SoundManager.hx
+++ src/me/miltage/SoundManager.hx
@@ -56,8 +56,8 @@ class SoundManager {
 
     public static function startMusic():Void
     {
-        //if (musicResource != null && !musicPlaying)
-            //musicResource.play(true, 0.5);
+        if (musicResource != null && !musicPlaying)
+            musicResource.play(true, 0.5);
         musicPlaying = true;
     }
 
